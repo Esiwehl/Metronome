1b7b2940251c8a89116021c85a99950ef3ff7e26
Esiwehl <59033264+Esiwehl@users.noreply.github.com>
2025-07-11 11:29:37 +0200

    utils for metronome.cpp

---
+++ utils.h
@@ -0,0 +1,12 @@
+#pragma once
+
+#include <iostream>
+#include <thread>
+#include <chrono>
+
+#include <fcntl.h> // Contains file controls like O_RDWR
+#include <errno.h> // Error integer and strerror() function
+#include <termios.h> // Contains POSIX terminal control definitions
+#include <unistd.h> // write(), read(), close()
+
+#include "colors.h"
